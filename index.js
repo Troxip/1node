57f75e14c513f1c56f433447a56517ca3fcb6e73
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-05-05 17:16:19 +0200

    Async

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const fs = require("fs");
 
-const hello = "Hello world";
-console.log(hello);
+// const testIn = fs.readFileSync("./txt/input.txt", "utf-8");
+
+// const textOut = `This is what we know about the avocado: ${testIn}`;
+// console.log(textOut);
+
+// Non Blocking
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
